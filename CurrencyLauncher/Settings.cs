78f768cc831a6300e3e2dc53b90fdb1097e324de
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-09-09 07:36:13 -0300

    Added settings
    Added settings class with settings.cfg file and SteamLocation and DebugMode as variables.

---
+++ CurrencyLauncher/Settings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyLauncher
+{
+	internal class Settings
+	{
+		public static Settings? Instance;
+
+		private readonly string location = "launcher.cfg";
+
+		public string SteamLocation = "";
+		public bool DebugMode = false;
+
+		internal Settings()
+		{
+			Instance = this;
+			Load();
+		}
+
+		private void Load()
+		{
+			if (!File.Exists(location))
+			{
+				return;
+			}
+			string[] fLines = File.ReadAllLines(location);
+			SteamLocation = fLines[0];
+			DebugMode = bool.Parse(fLines[1]);
+		}
+
+		public void Save()
+		{
+			File.WriteAllLines(location, new string[]{
+				SteamLocation,
+				DebugMode.ToString()
+			});
+		}
+	}
+}
